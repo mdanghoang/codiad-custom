8ab397318d34b1feac23cc02a783fa866b87ab69
mdanghoang <mdanghoang+dev@gmail.com>
2013-03-26 20:45:58 +0700

    update deploy script

--- components/shell_script/script/deploy_app_php.sh
+++ components/shell_script/script/deploy_app_php.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo "Deploy application"
 user=$1
 project_name=$2
-echo "Deploy project $project_name for user $user"
+git_server="git@10.186.0.247"
 name=`echo ${project_name##*_}`
-echo "Clone application from git server to /var/www/$name"
 git clone $git_server:$user/$project_name.git /var/www/$name
 chown -R www-data:www-data /var/www/$name
-echo "Deploy DONE"
+
+python /home/Workspace/codiad/src/deploy.py $name
