1a0d741e8ca7524ff48a9fedf5fc2066c17f1d3b
mdanghoang <mdanghoang+dev@gmail.com>
2013-03-26 20:25:02 +0700

    add some echo to console in deploy app script

--- components/shell_script/script/deploy_app_php.sh
+++ components/shell_script/script/deploy_app_php.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo "Deploy application"
 user=$1
 project_name=$2
+echo "Deploy project $project_name for user $user"
 name=`echo ${project_name##*_}`
+echo "Clone application from git server to /var/www/$name"
 git clone $git_server:$user/$project_name.git /var/www/$name
 chown -R www-data:www-data /var/www/$name
+echo "Deploy DONE"
